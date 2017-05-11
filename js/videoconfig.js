3f47498da76741ade18e2420e4b5116a6201707b
chrismillsnbcu <chris.mills@nbcuni.com>
2017-05-11 13:49:34 -0700

    init

---
+++ js/videoconfig.js
@@ -0,0 +1,28 @@
+<!--
+s.contextData["tlmd.videocat"] = s.getVideoCat(); s.contextData["tlmd.videosubcat"] = s.getVideoSubCat(); s.contextData["tlmd.userAgent"] = navigator.userAgent; s.contextData["tlmd.visitorType"] = s.getNewRepeat(); s.contextData["tve_domain"] = "telemundo.com"; s.contextData["tve_date"]="05/11/2017";s.contextData["tve_day"]="jueves";s.contextData["tve_hour"]="14";s.contextData["tve_minute"]="14:50"; s.contextData["tve_platform"] = jQuery.browser.mobile ? "Mobile" : "PC";if(document.domain.indexOf("nbcuxd.com") > -1 || location.href.indexOf("/now") > -1 || document.domain.indexOf("nbcuni.com") > -1) {  s_account = Drupal.settings.analytics.tveSuiteId;}
+s.linkInternalFilters="javascript:,this-site.com";
+s.contextData["tlmd.division"]="Hispanic Enterprises";
+s.contextData["tlmd.businessUnit"]="Telemundo.com";
+s.contextData["tlmd.publishDate"]="";
+s.contextData["tlmd.publishDay"]="";
+s.contextData["tlmd.publishMonth"]="";
+s.contextData["tlmd.publishYear"]="";
+s.contextData["tve_month"]="mayo";
+s.contextData["tve_year"]="2017";
+s.contextData["tlmd.url"]="http:\/\/telemundo.apps.nbcuni.com\/home";
+s.channel="Mainsite";
+s.contextData["tlmd.showSite"]="Telemundo.com - Homepage";
+s.contextData["tlmd.contentType"]="Main";
+s.contextData["tlmd.userGroup"]="anonymous user";
+s.contextData["tlmd.showFeature"]="Main";
+s.contextData["tlmd.showSubFeature"]="Main";
+s.pageName="Telemundo | Novelas, Shows, Deportes y Entretenimiento";
+s.contextData["tlmd.node"]="";
+s.prop8="Hispanic Enterprises";
+s.prop9="Telemundo.com";
+s.evar45="Telemundo.com";
+s.prop10="Telemundo.com - Homepage";
+s.evar36="";
+s.contextData["tlmd.pageCount"]="true";
+/************* DO NOT ALTER ANYTHING BELOW THIS LINE ! **************/
+var s_code=s.t();if(s_code)document.write(s_code)//-->
